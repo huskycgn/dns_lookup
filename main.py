81c7c6297011bd9cff9066e7c23ce35878f17b1c
joachimlehmann <joachim.lehmann@googlemail.com>
2022-10-29 15:24:41 +0200

    File handling is now a function

--- main.py
+++ main.py
@@ -8,24 +8,37 @@ my_resolver = dns.resolver.Resolver()
 
 print(art.image)
 
-try:
-    with open(file='servers.txt', mode='r') as server_file:
-        dns_list = server_file.read().splitlines()
-except FileNotFoundError:
-    dns_list = ['8.8.8.8', '8.8.4.4', '192.168.178.1']
-    print('No "servers.txt" found, using default DNS-Server list:')
-    for i in dns_list:
-        print(i)
+
+# Open servers.txt - create new servers.txt if none is found
+
+def open_file():
+    try:
+        with open(file='servers.txt', mode='r') as server_file:
+            dns_list = server_file.read().splitlines()
+            return dns_list
+    except FileNotFoundError:
+        dns_list = ['8.8.8.8', '8.8.4.4', '192.168.178.1']
+        print('No "servers.txt" found, using default DNS-Server list:\n'
+              'and creating new servers.txt')
+        with open(file='servers.txt', mode='w') as server_file:
+            for i in dns_list:
+                server_file.writelines(f'{i}\n')
+                print(i)
+            return dns_list
+
+
+# Calling function for servers.txt file
+
+dns_server_list = open_file()
 
 # getting user input
 
 hostname = input('Please enter a hostname (e.g. google.com):\n')
-
 records_dict_list = []
 
 # iterate through server list
 
-for server in dns_list:
+for server in dns_server_list:
     try:
         my_resolver.nameservers = [server]
         answers = my_resolver.resolve(hostname, 'A')
